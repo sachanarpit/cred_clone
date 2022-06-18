f245d201e7ff5ac915b25e0f79af080e988958aa
Arpit Sachan <sachanarpitknp@gmail.com>
2022-06-18 22:02:04 +0000

    After header created

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,10 +1,12 @@
 import Styles from "./index.module.scss";
+import { AfeterHeader } from "./partials/AfterHeader/AfterHeader";
 import { Header } from "./partials/Header/Header";
 
 export const RootComp = () => {
   return (
     <div className={Styles.Main_Container}>
       <Header />
+      <AfeterHeader />
     </div>
   );
 };
---
+++ src/components/partials/AfterHeader/AfterHeader.jsx
@@ -0,0 +1,14 @@
+import Styles from "./AfterHeader.module.scss";
+
+export const AfeterHeader = () => {
+  return (
+    <>
+      <div className={Styles.AfeterHeader}>
+        <p>
+          pay credit card bill. earn guaranteed ₹200 back.{" "}
+          <span> claim now </span>
+        </p>
+      </div>
+    </>
+  );
+};
